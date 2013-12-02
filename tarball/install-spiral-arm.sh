cbf4ce8b2fe8889a002d3f31711d02c5e0691eed
jpegleg <keegan.bowen@gmail.com>
2013-12-02 01:15:19 -0700

    Removed duplicated folder

--- tarball/install-spiral-arm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-rm -rf /var/opt/spiral-arm
-mkdir /var/opt/spiral-arm
-tar xzf spiralarm.tar.gz -C /var/opt/spiral-arm/
-chmod 644 /var/opt/spiral-arm
-chown root:root /var/opt/spiral-arm
