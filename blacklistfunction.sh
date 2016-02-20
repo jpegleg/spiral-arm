b1acc3204c4cfcf4541c03def7dfcd44e60eed8e
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-20 02:37:56 -0700

    Update blacklistfunction.sh

--- blacklistfunction.sh
+++ blacklistfunction.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ps aux | grep ssh | grep -v grep | grep -v root | awk '{print $2}' > /var/opt/spiral-arm/blacklist
-BLACKPIDS=$(diff /var/opt/spiral-arm/whitelist  /var/opt/spiral-arm/blacklist | grep "\> *" | grep -v "<" | awk '{print $2}')
-echo $BLACKPIDS > /var/opt/spiral-arm/black.pid
+ps aux | grep ssh | grep -v grep | grep -v root | awk '{print $2}' > /opt/spiral-arm/blacklist
+BLACKPIDS=$(diff /opt/spiral-arm/whitelist  /opt/spiral-arm/blacklist | grep "\> *" | grep -v "<" | awk '{print $2}')
+echo $BLACKPIDS > /opt/spiral-arm/black.pid
