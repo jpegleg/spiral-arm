e3b36fe2e5c1c85e9c61d662d19ab8ae03df8f2e
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-20 03:07:37 -0700

    Delete whitelistfunction.sh

--- spiral-arm/whitelistfunction.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-ps aux | grep ssh | grep -v grep | awk '{print $2}'> /var/opt/spiral-arm/whitelist
-chown root:root /var/opt/spiral-arm/whitelist
-chown root:root /var/opt/spiral-arm
