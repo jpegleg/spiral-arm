2de4bec61aa38f130ef5b5b6fc67b80fefc50559
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-20 02:44:42 -0700

    Update whitelistfunction.sh

--- whitelistfunction.sh
+++ whitelistfunction.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ps aux | grep ssh | grep -v grep | awk '{print $2}'> /var/opt/spiral-arm/whitelist
-chown root:root /var/opt/spiral-arm/whitelist
-chown root:root /var/opt/spiral-arm
+ps aux | grep ssh | grep -v grep | awk '{print $2}'> /opt/spiral-arm/whitelist
+chown root:root /opt/spiral-arm/whitelist
+chown root:root /opt/spiral-arm
